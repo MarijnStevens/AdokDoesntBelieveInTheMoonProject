0f54c5d2ea742e6b7f9bed47c91cc61bdfc55425
Marijn Stevens <marijnstevens@gmail.com>
2023-10-07 17:25:52 +0200

    Small update to publish.

--- publish.sh
+++ publish.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
+set -e
 
-# Linux build
-dotnet publish -c Release -r linux-x64 --self-contained true \
+## Linux build
+OS="linux-x64"
+BUILDDIR="build/$OS"
+BINARY="./${BUILDDIR}/Myriad"
+_BE_FOR_STRIP=0
+_BE_AFTER_STRIP=0
+
+
+clean() {
+    rm -r build/$1 2> /dev/null
+}
+
+publish() {
+    dotnet publish -c Release -o $BUILDDIR -r $1 --self-contained true \
 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true \
 -p:PublishTrimmed=true -p:TrimMode=link
+}
 
-# # MacOS build
-# dotnet publish -c Release -r osx-x64 --self-contained true \ 
-# -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true \
-# -p:PublishTrimmed=True -p:TrimMode=link
+publish $OS &&\
+strip $BINARY 
 
-# # Windows build.
-# dotnet publish -c Release -r win-x64 --self-contained true \
-# -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true \ 
-# -p:PublishTrimmed=True -p:TrimMode=link
+## MacOS build
+# publish osx-x64
 
-
+## Windows build.
+# publish win-x64
